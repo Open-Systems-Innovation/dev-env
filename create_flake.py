02bf539e71b95c77e9127bc63326f33c577f535d
Open-Systems-Innovation <info@opensystemsinnovation.com>
2024-05-23 13:38:26 -0400

    first commit

---
+++ create_flake.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+# Path to the source flake.nix file
+source_flake_path = os.path.join(os.path.dirname(__file__), 'flake.nix')
+
+# Destination paths
+dest_flake_path = os.path.join(os.getcwd(), 'flake.nix')
+dest_envrc_path = os.path.join(os.getcwd(), '.envrc')
+
+# Copy flake.nix to the current directory
+shutil.copyfile(source_flake_path, dest_flake_path)
+
+# Content for .envrc
+envrc_content = 'use flake'
+
+# Create .envrc file
+with open(dest_envrc_path, 'w') as f:
+    f.write(envrc_content.strip())
+
+# Run direnv allow
+os.system('direnv allow')
