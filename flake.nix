01bab24405965bff38ee464863c35d559a6dca23
Open-Systems-Innovation <info@opensystemsinnovation.com>
2024-05-26 18:38:08 -0400

    Removed all useage of flate-parts and nix-systems

--- flake.nix
+++ flake.nix
@@ -2,56 +2,43 @@
   description = "Description for the project";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    systems.url = "github:nix-systems/default";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    custom-nixpkgs.url = "github:open-systems-innovation/custom-nixpkgs";
+    custom-nixpkgs.url = "github:Open-Systems-Innovation/custom-nixpkgs";
   };
 
   outputs =
-    inputs@{ flake-parts, systems, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import systems;
-
-      imports = [ ./imports/overlay.nix ];
-
-      perSystem =
+    { self, nixpkgs, custom-nixpkgs, ... }:
+      let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; overlays = [ custom-nixpkgs ]};
+      in
         {
-          config,
-          self',
-          inputs',
-          pkgs,
-          system,
-          ...
-        }:
-        {
-          devShells.default = pkgs.mkShellNoCC {
-            name = "default";
-            
-            packages = [
-              # General packages
-              pkgs.hello-nix
+        devShells.default = pkgs.mkShellNoCC {
+          name = "default";
+             
+          packages = [
+          # General packages
+            pkgs.hello-nix
 
-              # Python packages
-              (pkgs.python3.withPackages (python-pkgs: [
-                # packages for formatting/ IDE
-                python-pkgs.black
-                python-pkgs.flake8
-                python-pkgs.jedi
-                python-pkgs.pip
-                python-pkgs.setuptools
-                # packages for code
-                python-pkgs.gmsh
-                python-pkgs.matplotlib
-                python-pkgs.meshio
-                python-pkgs.numpy
-              ]))
-            ];
+            # Python packages
+            (pkgs.python3.withPackages (python-pkgs: [
+              # packages for formatting/ IDE
+              python-pkgs.black
+              python-pkgs.flake8
+              python-pkgs.jedi
+              python-pkgs.pip
+              python-pkgs.setuptools
+              # packages for code
+              python-pkgs.gmsh
+              python-pkgs.matplotlib
+              python-pkgs.meshio
+              python-pkgs.numpy
+            ]))
+          ];
 
-            shellHook = ''
+          shellHook = ''
               export PS1="\[\e[0;32m\]dev-shell>\[\e[0;36m\] "
-            '';
-          };
+          '';
         };
-    };
+        };
 }
