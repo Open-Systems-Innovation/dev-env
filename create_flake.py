c011ad61b7b27bd7c1fd9695876367428a534131
Open-Systems-Innovation <info@opensystemsinnovation.com>
2024-05-28 14:59:51 -0400

    improved python logic

--- create_flake.py
+++ create_flake.py
@@ -3,8 +3,12 @@
 import os
 import shutil
 
+# Get the current working directory
+current_dir = os.getcwd()
+
 # Path to the source flake.nix file
-source_flake_path = os.path.join(os.path.dirname(__file__), 'flake.nix')
+
+source_flake_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'flake.nix')
 
 # Destination paths
 dest_flake_path = os.path.join(os.getcwd(), 'flake.nix')
